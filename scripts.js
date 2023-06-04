e1b572ac68147ade571b8070b97d8bf059502e5a
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2023-06-04 16:22:53 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,14 +7,28 @@ function copyMenu(){
 
     //copy inside nav to nav
     var mainNav = document.querySelector('.header-nav nav');
-    var navplace = document.querySelector('off-canvas nav')
+    var navplace = document.queySelector('off-canvas nav')
     navplace.innerHTML = mainNav.innerHTML;
 
     var topNav = document.querySelector('.header-top .wrapper');
     var topPlace = document.querySelector('.off-canvas .thetop-nav')
     topPlace.innerHTML = topNav.innerHTML;
-}
+}  
 copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector('.trigger'),
+      closeButton = document.querySelector('.t-close'),
+      addClass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+    addClass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function(){
+    addClass.classList.remove('showmenu')
+})
+closeButton.addEventListener('click', function(){
+    addClass.classList.remove('showmenu')
+})
 
 //show sub menu on mobile
 const submenu = document.querySelectorAll('.has-child .icon-small');
@@ -24,5 +38,26 @@ function toggle(e){
     e.preventDefault();
     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand'): null);
     if (this.closest('.has-child').classList != 'expand');
-    this.closest('.has-child').classList.toggle('expand ')
+    this.closest('.has-child').classList.toggle('expand');
+
 }
+//slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
+
+//show search
+const seachButton = document.querySelector('.t-search'),
+    tClose = document.querySelector('.search-close'),
+    showClass = document.querySelector('.site');
+searchButton.addEventListener('click', function(){
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function(){
+    showClass.classList.remove('showsearch')
+})

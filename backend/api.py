00b1b6b753850935991981d9b8bb848bba83d3bd
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-06-05 00:46:48 +0700

    Add files via upload

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +7,11 @@ from typing import List
 from database import SessionLocal
 from fastapi.staticfiles import StaticFiles 
 from fastapi.responses import FileResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import Depends
 
 # Database configuration
 DATABASE_URL = "sqlite:///./users.db"
@@ -37,7 +42,6 @@ class UserLogin(BaseModel):
     email: str
     password: str
 
-
 # FastAPI app
 app = FastAPI()
 
@@ -52,17 +56,65 @@ def get_db():
 # Serve static files (HTML, CSS, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# OAuth2 configuration
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Helper functions
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+from sqlalchemy.orm import joinedload
+
+def authenticate_user(email: str, password: str, db: Session):
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not verify_password(password, user.password):
+        return None
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 # API endpoints
+@app.post("/token")
+def get_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email},
+        expires_delta=access_token_expires,
+    )
+    
+    return {"access_token": access_token, "token_type": "bearer"}
+
 @app.get("/users/all", response_model=List[UserResponse])
 def get_all_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     user_responses = [UserResponse(id=user.id, email=user.email, address=user.address) for user in users]
     return user_responses
 
-
 @app.post("/users", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = User(email=user.email, password=user.password, address=user.address)
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, password=hashed_password, address=user.address)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -72,7 +124,6 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         address=db_user.address
     )
     return user_response
-
 
 @app.get("/users/{user_id}", response_model=UserResponse)
 def get_user(user_id: int, db: Session = Depends(get_db)):
@@ -93,13 +144,19 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/login")
 def login_user(user_login: UserLogin, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == user_login.email).first()
-    if user is None or user.password != user_login.password:
+    user = authenticate_user(user_login.email, user_login.password, db)
+    if not user:
         raise HTTPException(status_code=401, detail="Invalid email or password")
-    return {"message": "Login successful"}
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email},
+        expires_delta=access_token_expires,
+    )
+    
+    return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.get("/signup")
 def signup_page():
     return FileResponse("static/signup.html")
-
-
